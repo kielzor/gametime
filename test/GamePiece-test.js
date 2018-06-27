3d91b5871d71343fc7e5a1f99f3582a110db186d
Kiel Dehnert <kielzor@gmail.com>
2018-06-27 12:50:54 -0600

    add first tests

---
+++ test/GamePiece-test.js
@@ -0,0 +1,50 @@
+const { assert } = require('chai');
+const GamePiece = require('../lib/GamePiece.js');
+
+describe('GamePiece', function () {
+
+  it('should have properties', function () {
+    const gamePiece = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0)', 1);
+    const expectedObj = {
+      x: 50,
+      y: 50,
+      height: 10, 
+      width: 10,
+      color: 'rgb(250, 0, 0)',
+      dx: 1,
+      dxv: .5
+    };
+
+    assert.deepEqual(gamePiece, expectedObj);
+  });
+
+  it('should be able to collide with other objects', function () {
+    const gamePiece1 = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0)', 1);
+    const gamePiece2 = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0)', 1);
+
+    const isColliding = gamePiece1.isCollidingWith(gamePiece2);
+
+    assert.isTrue(isColliding);
+  })
+
+  it('should be able to collide with other objects', function () {
+    const gamePiece1 = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0)', 1);
+    const gamePiece2 = new GamePiece(70, 70, 10, 10, 'rgb(250, 0, 0)', 1);
+
+    const isColliding = gamePiece1.isCollidingWith(gamePiece2);
+
+    assert.isFalse(isColliding);
+  })
+
+  it('should move', function() {
+    const gamePiece1 = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0)', 1);
+  
+    gamePiece1.move();
+
+    assert.equal(gamePiece1.x, 50.5);
+
+    gamePiece1.move();
+
+    assert.equal(gamePiece1.x, 51);
+  })
+})
