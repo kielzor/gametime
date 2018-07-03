1d56ad73fc837c478fc5df0c1429f222d9f8cbf1
Tim Fischer <emailtimfischer@gmail.com>
2018-07-02 20:49:52 -0600

    Create is function and instantiate tests for fruit

--- test/Fruit-test.js
+++ test/Fruit-test.js
@@ -2,6 +2,14 @@ const { assert } = require('chai');
 const Fruit = require('../lib/Fruit.js');
 
 describe('Fruit', function () {
+  it('should be a function', function () {
+    assert.isFunction(Fruit);
+  })
+  it('should instantiate a piece of fruit', function(){
+    var fruit = new Fruit();
+    assert.isObject(fruit);
+  })
+
 
   it('should have properties', function () {
     var fruit = new Fruit();
